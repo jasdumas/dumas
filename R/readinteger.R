4cbc3bf2af4406757065e9af9535501e88e32091
Jasmine Dumas <jasmine.dumas@gmail.com>
2015-08-26 20:28:14 -0400

    comment out functions that prompt users for input

--- R/readinteger.R
+++ R/readinteger.R
@@ -1,9 +1,9 @@
-## A Sample function that prompts the user for a number and returns a integer
-
-readinteger <- function()
-{
-  n <- readline(prompt="Enter an integer: ")
-  return(as.integer(n))
-}
-
-print(readinteger())
+# ## A Sample function that prompts the user for a number and returns a integer
+#
+# readinteger <- function()
+# {
+#   n <- readline(prompt="Enter an integer: ")
+#   return(as.integer(n))
+# }
+#
+# print(readinteger())
