6dc45cd2d1c9db8feaf90dff3d68a5d20238ad51
Jasmine Dumas <jasdumas@users.noreply.github.com>
2015-12-27 11:59:30 -0500

    Create code_block.R :speech_balloon:
    for a one line code block...maybe I'll include another function for multiline comments soon

---
+++ R/code_block.R
@@ -0,0 +1,6 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+#  a series of one-liner functions / R Code 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+code_block = function(text) { l = nchar(text) cat(replicate(l, "#"), "\n", "# ", text, "\n", replicate(l, "#"))}
