2a5d5be18471f6c758e15ed490cc32bb7ed4fda0
Jasmine Dumas <jasdumas@users.noreply.github.com>
2016-02-19 10:27:39 -0500

    Added files via upload
    SO function

---
+++ R/cbind.fill.R
@@ -0,0 +1,9 @@
+# http://stackoverflow.com/questions/7962267/cbind-a-df-with-an-empty-df-cbind-fill
+
+cbind.fill <- function(...){
+  nm <- list(...) 
+  nm <- lapply(nm, as.matrix)
+  n <- max(sapply(nm, nrow)) 
+  do.call(cbind, lapply(nm, function (x) 
+    rbind(x, matrix(, n-nrow(x), ncol(x))))) 
+}
