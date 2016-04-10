e14a3e12b05551086ce6ece04ee11b17f361127b
Jasmine Dumas <jasmine.dumas@gmail.com>
2016-04-10 10:14:59 -0400

    adding ?documentation for hello and open.this

--- R/hello.R
+++ R/hello.R
@@ -1,3 +1,12 @@
+#' Hello!
+#'
+#' This is a starter function to just say hello!
+#' @param  just say hi
+#' @return "Hi, I'm Jasmine"
+#' @export
+#'
+#' @examples
+#' hello()
 hello <- function(){
   print("Hi, I'm Jasmine")
 }
--- R/open.this.R
+++ R/open.this.R
@@ -1,6 +1,16 @@
-## Function graciously acquired from Garrett Dancik, PhD @ ECSU all credit to my mentor
-## This opens a dataset in a tempfile in excel for cleaner visuals of the data
-
+#' Title
+#'
+#' This opens a dataset in a tempfile in excel for cleaner visuals of the data
+#' @param x dataset or object of class data.frame
+#' @param file default file append
+#'
+#' @return "press enter to continue..."
+#' @export
+#'
+#' @examples
+#' open.this()
+#' @seealso
+#' Function graciously acquired from Garrett Dancik, PhD @ ECSU
 open.this <-function(x, file = "__tmp.csv") {
   write.table(x, file = file, sep = ",", row.names = TRUE, col.names = NA)
   cmd = paste("open ", file, sep = "")
