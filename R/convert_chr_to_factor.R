3b1fc11975831dcbf3541fa079014893a6721dac
Jasmine Dumas <jasdumas@users.noreply.github.com>
2016-08-15 09:37:27 -0400

    Update convert_chr_to_factor.R

--- R/convert_chr_to_factor.R
+++ R/convert_chr_to_factor.R
@@ -9,7 +9,7 @@
 #' @examples
 #' @details http://stackoverflow.com/a/2851213
 convert_chr_to_factor <- function(df, ...){
-  df <<- data.frame()
-  df[] <- lapply(df, as.character)
-  df <<- df
+  character_vars <- lapply(df, class) == "character"
+  df[, character_vars] <- lapply(df[, character_vars], as.factor)
+  return(df)
 }
